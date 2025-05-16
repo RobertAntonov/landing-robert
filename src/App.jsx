531ec9b33559a6e8b1a9a7b269e97096861d0885
Robert Antonov <robert.antonov0@gmail.com>
2025-05-16 16:11:23 +0300

    added missing index.css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Landing Page Robert</h1>
+      <p>Site-ul funcționează 🟢</p>
+    </div>
+  );
+};
+
+export default App;
+
