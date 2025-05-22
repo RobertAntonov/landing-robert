b88f4fe3996390a7806aeb7a6782f579de7509bc
Robert Antonov <robert.antonov0@gmail.com>
2025-05-22 15:07:18 +0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
 import ThemeToggle from "./components/ThemeToggle";
-import CometManager from "./components/CometManager";
 
 const App = () => {
   return (
     <div className="min-h-screen relative">
       <ThemeToggle />
-      <CometManager />
       <Hero />
       <About />
     </div>
